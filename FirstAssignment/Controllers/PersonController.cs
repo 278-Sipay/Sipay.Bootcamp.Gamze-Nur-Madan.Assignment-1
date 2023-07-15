c60da4af4cb0cffa504fed6a85a75f0e716447ea
gamzenurmadan <gamzenurmadan@outlook.com>
2023-07-15 11:49:32 +0300

    Add project files.

---
+++ FirstAssignment/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using FirstAssignment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        public PersonController() { }
+
+
+        [HttpPost]
+        public Person Post([FromBody] Person person)
+        {
+            return person;
+        }
+    }
+}
